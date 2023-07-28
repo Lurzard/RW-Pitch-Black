64075aa67cdf74d1ff7095c5490c9a0b3bfc8d8c
birdcatt <avescatus@gmail.com>
2023-07-28 12:16:41 -0400

    Beacon flarebomb > slugOnBack priority
    Beacon:
    - Prioritizes throwing stored flarebombs over throwing slug on back
    - Prioritizes putting flarebombs from hand into storage over putting slugpups onto free hand
    
    Flarebomb:
    - Fixed creature check. Players no longer get stunned from flarebombs
    - Added additional condition for flarebomb stun that also checks if any Jolly Co-op players are either Beacon or Photo

--- PitchBlack-master/src/ScugCode/BeaconHooks.cs
+++ PitchBlack-master/src/ScugCode/BeaconHooks.cs
@@ -1,4 +1,6 @@
 using AbstractObjectType = AbstractPhysicalObject.AbstractObjectType;
+using MonoMod.Cil;
+using Mono.Cecil.Cil;
 
 namespace PitchBlack;
 
@@ -6,20 +8,94 @@ public static class BeaconHooks
 {
     public static void Apply()
     {
+        //spinch: i also want to hook to Player.ThrowToGetFree to make beacon throw flashbangs if grasps are empty
+
+        IL.Player.GrabUpdate += Player_GrabUpdate_IL;
         On.Player.Die += Player_Die;
-        //On.Player.ThrowToGetFree += Player_ThrowToGetFree;
         On.Player.Jump += Player_Jump;
         On.Player.SwallowObject += BeaconTransmuteIntoFlashbang;
         On.Player.GrabUpdate += Player_GrabUpdate;
         On.Player.GraphicsModuleUpdated += BeaconStorageGrafUpdate;
     }
 
+    private static void Player_GrabUpdate_IL(ILContext il)
+    {
+        //spinch: give priority to auto-throwing flarebombs from storage over throwing slug on back
+        ILCursor c = new(il);
+        ILLabel label = il.DefineLabel();
+            
+        if (!c.TryGotoNext(MoveType.After,
+            i => i.MatchCallOrCallvirt<Player.SlugOnBack>("get_HasASlug"),
+            i => i.MatchBrfalse(out label)
+            ))
+        {
+            return;
+        }
+
+        //if (!c.TryGotoPrev(MoveType.Before,
+        //    i => i.MatchLdsfld<ModManager>(nameof(ModManager.MSC))
+        //    ))
+        //{
+        //    return;
+        //}
+
+        #region
+        //c.Emit(OpCodes.Ldarg_0);
+        //c.EmitDelegate((Player self) =>
+        //{
+        //    if (!Plugin.scugCWT.TryGetValue(self, out var cwt) || !cwt.IsBeacon)
+        //        return;
+
+        //    if (cwt.Beacon.storage.storedFlares.Count <= 0)
+        //        return;
+
+        //    if (self.slugOnBack == null || !self.slugOnBack.HasASlug)
+        //        return;
+
+        //    foreach (var item in self.grasps)
+        //    {
+        //        if (item != null && self.IsObjectThrowable(item.grabbed))
+        //        {
+        //            return;
+        //        }
+        //    }
+
+        //    self.wantToThrow = 0;
+        //});
+        #endregion
+
+        c.Emit(OpCodes.Ldarg_0);
+        c.EmitDelegate((Player self) =>
+        {
+            if (!Plugin.scugCWT.TryGetValue(self, out var cwt) || !cwt.IsBeacon)
+                return false;
+
+            if (self.slugOnBack == null || !self.slugOnBack.HasASlug)
+                return false;
+
+            if (cwt.Beacon.storage.storedFlares.Count <= 0)
+                return false;
+
+            foreach (var item in self.grasps)
+            {
+                if (item != null && self.IsObjectThrowable(item.grabbed))
+                {
+                    return false;
+                }
+            }
+
+            //self.wantToThrow = 0;
+            return true;
+        });
+        c.Emit(OpCodes.Brtrue, label);
+    }
+
     private static void Player_Die(On.Player.orig_Die orig, Player self)
     {
         orig(self);
+        //spinch: on death, all of beacon's stored flarebombs gets popped off
         if (Plugin.scugCWT.TryGetValue(self, out var cwt) && cwt.Beacon?.storage != null)
         {
-            //spinch: on death, all of beacon's stored flarebombs gets popped off
             while (cwt.Beacon.storage.storedFlares.Count > 0)
             {
                 FlareBomb fb = cwt.Beacon.storage.storedFlares.Pop();
@@ -32,38 +108,6 @@ public static class BeaconHooks
             }
         }
     }
-
-#if false
-    private static void Player_ThrowToGetFree(On.Player.orig_ThrowToGetFree orig, Player self, bool eu)
-    {
-        //spinch: doesnt work & i dont feel like coding anymore today
-        if (Plugin.scugCWT.TryGetValue(self, out ScugCWT cwt) && cwt.IsBeacon)
-        {
-            bool dontThrowFlarebomb = self.FreeHand() == -1;
-            if (Plugin.BeaconName == self.slugcatStats.name)
-            {
-                for (int i = 0; i < 2; i++)
-                {
-                    if (self.grasps[i]?.grabbed is Weapon)
-                    {
-                        dontThrowFlarebomb = true;
-                        break;
-                    }
-                }
-            }
-
-            if (!dontThrowFlarebomb && cwt.Beacon.storage.storedFlares.Count > 0)
-            {
-                //auto throw flarebomb on an empty hand
-                int freeHand = self.FreeHand();
-                cwt.Beacon.storage.FlarebombFromStorageToPaw(eu);
-                self.ThrowObject(freeHand, eu);
-            }
-        }
-
-        orig(self, eu);
-    }
-#endif
 
     private static void Player_Jump(On.Player.orig_Jump orig, Player self)
     {
@@ -99,16 +143,27 @@ public static class BeaconHooks
     }
     public static void Player_GrabUpdate(On.Player.orig_GrabUpdate orig, Player self, bool eu)
     {
+        bool slugIsBeacon = Plugin.scugCWT.TryGetValue(self, out ScugCWT cwt) && cwt.IsBeacon;
         bool dontAutoThrowFlarebomb = self.FreeHand() == -1;
-        if (Plugin.BeaconName == self.slugcatStats.name)
+
+        if (slugIsBeacon)
         {
             //spinch: check grasps to see if beacon is holding a throwable object or a creature
             // if true, then don't auto throw the flarebombs from storage
             // done before orig because things can get thrown during orig
             for (int i = 0; i < 2; i++)
             {
-                if (self.grasps[i]?.grabbed != null && (self.IsObjectThrowable(self.grasps[i].grabbed) || self.grasps[i].grabbed is Creature))
+                if (self.grasps[i] != null && (self.IsObjectThrowable(self.grasps[i].grabbed) || self.grasps[i].grabbed is Creature))
                 {
+                    if (self.slugOnBack != null
+                        && self.input[0].pckp
+                        && self.grasps[i].grabbed is FlareBomb
+                        && cwt.Beacon.storage.storedFlares.Count < cwt.Beacon.storage.capacity)
+                    {
+                        //if you're trying to put a flarebomb into storage, don't put the slug on back to your hand
+                        self.slugOnBack.interactionLocked = true;
+                        self.slugOnBack.counter = 0;
+                    }
                     dontAutoThrowFlarebomb = true;
                     break;
                 }
@@ -117,22 +172,22 @@ public static class BeaconHooks
 
         orig(self, eu);
 
-        if (Plugin.scugCWT.TryGetValue(self, out ScugCWT cwt) && cwt.IsBeacon)
+        if (slugIsBeacon)
         {
-            bool interactionLockStorage = self.eatMeat > 0;
-            if (!interactionLockStorage)
+            bool interactionLockFlarebombStorage = self.eatMeat > 0;
+            if (!interactionLockFlarebombStorage)
             {
                 //check grasps for food if not eating meat
                 for (int i = 0; i < 2; i++)
                 {
                     if (self.grasps[i]?.grabbed is IPlayerEdible)
                     {
-                        interactionLockStorage = true;
+                        interactionLockFlarebombStorage = true;
                         break;
                     }
                 }
             }
-            if (interactionLockStorage)
+            if (interactionLockFlarebombStorage)
             {
                 //dont take flarebomb from storage if holding food or eating
                 cwt.Beacon.storage.interactionLocked = true;
@@ -153,6 +208,7 @@ public static class BeaconHooks
                     //auto throw flarebomb on an empty hand
                     cwt.Beacon.storage.FlarebombFromStorageToPaw(eu);
                     self.ThrowObject(self.FreeHand(), eu);
+                    self.wantToThrow = 0;
                 }
             }
         }
