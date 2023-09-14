14610b322121ad24d50f871cc29f344207de25b5
Moon613 <68190337+Moon613@users.noreply.github.com>
2023-09-14 16:17:08 -0400

    Oh My God They're Dead

---
+++ PitchBlack-master/src/Utilities/OhNoMoonAndPebblesAreDeadGuys.cs
@@ -0,0 +1,21 @@
+using System;
+using MonoMod.RuntimeDetour;
+using MoreSlugcats;
+using static System.Reflection.BindingFlags;
+
+namespace PitchBlack;
+
+public class OhNoMoonAndPebblesAreDeadGuys
+{
+    public static void Apply() {
+        On.Oracle.Update += Oracle_Update;
+        // new Hook(typeof(SSOracleBehavior).GetMethod("get_EyesClosed", Public | NonPublic | Instance), (Func<SSOracleBehavior, bool> orig, SSOracleBehavior self) => (self.oracle.stun > 0 && self.oracle.room.game.session is StoryGameSession session && (session.saveStateNumber == Plugin.BeaconName || session.saveStateNumber == Plugin.PhotoName)) || orig(self));
+        new Hook(typeof(SSOracleRotBehavior).GetMethod("get_EyesClosed", Public | NonPublic | Instance), (Func<SSOracleRotBehavior, bool> orig, SSOracleRotBehavior self) => (self.oracle.stun > 0 && self.oracle.room.game.session is StoryGameSession session && (session.saveStateNumber == Plugin.BeaconName || session.saveStateNumber == Plugin.PhotoName)) || orig(self));
+    }
+    public static void Oracle_Update(On.Oracle.orig_Update orig, Oracle self, bool eu) {
+        if (self.room?.game.session is StoryGameSession session && (session.saveStateNumber == Plugin.BeaconName || session.saveStateNumber == Plugin.PhotoName) && (self.oracleBehavior is MoreSlugcats.SSOracleRotBehavior || self.oracleBehavior is SSOracleBehavior)) {
+            self.stun = 9999;
+        }
+        orig(self, eu);
+    }
+}
