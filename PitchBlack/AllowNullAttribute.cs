d68c780ebaf446b98c1fe43c150344ecaa64ab3c
Lurzard <scethasucks@gmail.com>
2023-09-17 17:34:36 -0400

    old obsolete code

--- PitchBlack/AllowNullAttribute.cs
+++
@@ -1,6 +0,0 @@
-﻿using System;
-
-namespace PitchBlack;
-
-[AttributeUsage(AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Parameter, Inherited = false)]
-public sealed class AllowNullAttribute : Attribute { }
