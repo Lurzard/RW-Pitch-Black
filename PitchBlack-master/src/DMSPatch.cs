ab400da7ef1c52d36680aeb570d5841e8dcce781
birdcatt <avescatus@gmail.com>
2023-09-17 00:32:21 -0400

    DMS sprite support for Photo's splatter + added pink's Beacon and Photo DMS skins

--- PitchBlack-master/src/DMSPatch.cs
+++ PitchBlack-master/src/DMSPatch.cs
@@ -8,13 +8,22 @@ internal class DMSPatch
     //DMS v1.6.6
     public static void AddSpritesToDMS()
     {
-        SpriteDefinitions.AddSprite(new SpriteDefinitions.AvailableSprite
+        SpriteDefinitions.AddSprite(new()
         {
             Name = "WHISKERS", //name at the top when clicking on the description box
             Description = "Whiskers", //description on dms menu
             GallerySprite = "LizardScaleA0",
             RequiredSprites = new() { "LizardScaleA0" },
             Slugcats = new() { BeaconName.value, PhotoName.value }
+        });
+
+        SpriteDefinitions.AddSprite(new()
+        {
+            Name = "SPLATTER",
+            Description = "Splatter",
+            GallerySprite = "PhotoSplatter",
+            RequiredSprites = new() { "PhotoSplatter" },
+            Slugcats = new() { PhotoName.value },
         });
     }
 }
