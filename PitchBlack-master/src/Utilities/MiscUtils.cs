012a17d4bdeb8b2c37dcfff7036bcb9e21dc41eb
Moon613 <68190337+Moon613@users.noreply.github.com>
2024-01-07 23:49:15 -0500

    More name checks

--- PitchBlack-master/src/Utilities/MiscUtils.cs
+++ PitchBlack-master/src/Utilities/MiscUtils.cs
@@ -4,19 +4,40 @@ namespace PitchBlack;
 
 public class MiscUtils
 {
+    #region Bacon or Photo checks
     public static bool IsBeaconOrPhoto(GameSession session)
     {
-        if (session is StoryGameSession s) {
-            return IsBeaconOrPhoto(s.saveStateNumber);
-        }
-        return false;
-    }
-    public static bool IsBeaconOrPhoto(SlugcatStats.Name slugName)
-    {
-        return null != slugName && (slugName == BeaconName || slugName == PhotoName);
+        return (session is StoryGameSession s) && IsBeaconOrPhoto(s.saveStateNumber);
     }
     public static bool IsBeaconOrPhoto(Creature crit)
     {
         return crit is Player player && IsBeaconOrPhoto(player.slugcatStats.name);
     }
+    public static bool IsBeaconOrPhoto(SlugcatStats.Name slugName)
+    {
+        return null != slugName && (slugName == BeaconName || slugName == PhotoName);
+    }
+    #endregion
+    #region Bacon Checks
+    public static bool IsBeacon(GameSession session) {
+        return (session is StoryGameSession s) && IsBeacon(s.saveStateNumber);
+    }
+    public static bool IsBeacon(Creature crit) {
+        return (crit is Player player) && IsBeacon(player.slugcatStats.name);
+    }
+    public static bool IsBeacon(SlugcatStats.Name name) {
+        return name != null && name == BeaconName;
+    }
+    #endregion
+    #region Photo Checks
+    public static bool IsPhoto(GameSession session) {
+        return (session is StoryGameSession s) && IsPhoto(s.saveStateNumber);
+    }
+    public static bool IsPhoto(Creature crit) {
+        return (crit is Player player) && IsPhoto(player.slugcatStats.name);
+    }
+    public static bool IsPhoto(SlugcatStats.Name name) {
+        return name != null && name == PhotoName;
+    }
+    #endregion
 }
