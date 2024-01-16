9eeca89ab25f6356f9d7d7176155a68599d8fd01
Moon613 <68190337+Moon613@users.noreply.github.com>
2024-01-16 03:06:34 -0500

    Updates to the code here
    Now it calls orig correctly... not sure how that didn't happen in the first place.

--- PitchBlack-master/src/Utilities/OhNoMoonAndPebblesAreDeadGuys.cs
+++ PitchBlack-master/src/Utilities/OhNoMoonAndPebblesAreDeadGuys.cs
@@ -10,17 +10,14 @@ public class OhNoMoonAndPebblesAreDeadGuys
     public static void Apply()
     {
         On.Oracle.Update += Oracle_Update;
-        new Hook(typeof(SSOracleRotBehavior).GetMethod("get_EyesClosed", Public | NonPublic | Instance), (Func<SSOracleRotBehavior, bool> orig, SSOracleRotBehavior self) => (self.oracle.stun > 0 && self.oracle.room.game.session is StoryGameSession session && (session.saveStateNumber == Plugin.BeaconName || session.saveStateNumber == Plugin.PhotoName)) || orig(self));
+        new Hook(typeof(SSOracleRotBehavior).GetMethod("get_EyesClosed", Public | NonPublic | Instance), (Func<SSOracleRotBehavior, bool> orig, SSOracleRotBehavior self) => (self.oracle.stun > 0 && MiscUtils.IsBeaconOrPhoto(self.oracle.room?.game?.session)) || orig(self));
     }
     public static void Oracle_Update(On.Oracle.orig_Update orig, Oracle self, bool eu)
     {
-        if (self.room.world.region != null && (self.room.world.region.name == "RM" || self.room.world.region.name == "LM"))
+        if (self.room?.world?.region != null && (self.room.world.region.name == "RM" || self.room.world.region.name == "LM") && MiscUtils.IsBeaconOrPhoto(self.room.game.session) && (self.oracleBehavior is SSOracleRotBehavior || self.oracleBehavior is SSOracleBehavior))
         {
-            if (self.room?.game.session is StoryGameSession session && (session.saveStateNumber == Plugin.BeaconName || session.saveStateNumber == Plugin.PhotoName) && (self.oracleBehavior is MoreSlugcats.SSOracleRotBehavior || self.oracleBehavior is SSOracleBehavior))
-            {
-                self.stun = 9999;
-            }
-            orig(self, eu);
+            self.stun = 9999;
         }
+        orig(self, eu);
     }
 }
