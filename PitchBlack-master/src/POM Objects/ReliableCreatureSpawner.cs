91e2ff30072494ee09d8bdcc8cc7c0d00defbf47
Moon613 <68190337+Moon613@users.noreply.github.com>
2024-04-04 17:34:47 -0400

    Actually I like it the way it was
    (this version actually breaks lol)

--- PitchBlack-master/src/POM Objects/ReliableCreatureSpawner.cs
+++ PitchBlack-master/src/POM Objects/ReliableCreatureSpawner.cs
@@ -16,67 +16,60 @@ public class CreatureSpawnerHooks
     private static void Room_Loaded(ILContext il)
     {
         try {
-            int local = 0;
             var cursor = new ILCursor(il);
             if (!cursor.TryGotoNext(MoveType.After, i => i.MatchLdsfld<PlacedObject.Type>(nameof(PlacedObject.Type.FlareBomb)))) {
                 Plugin.logger.LogDebug("Pitch Black IL Room Placedobjects failed 1");
                 return;
             }
-            if (cursor.TryGotoPrev(MoveType.After, i => i.MatchLdloc(out int local))) {
-                if (!cursor.TryGotoPrev(MoveType.After, i => i.MatchLdarg(0))) {
-                    Plugin.logger.LogDebug("Pitch Black IL Room Placedobjects failed 3");
-                    return;
-                }
-                try {
-                    cursor.Emit(OpCodes.Ldarg_0);
-                    cursor.Emit(OpCodes.Ldloc, local);
-                    cursor.EmitDelegate((Room self, int i) => {
-                        if (self.roomSettings.placedObjects[i].active && self.roomSettings.placedObjects[i].type == ReliableCreatureSpawner) {
-                            
-                            if (self.game.session is SandboxGameSession session && !session.PlayMode) {
-                                return;
-                            }
-
-                            string[] objectSettings = self.roomSettings.placedObjects[i].data.ToString().Split('~');
-
-                            CreatureTemplate.Type creatureTemplateType = (CreatureTemplate.Type)ExtEnum<CreatureTemplate.Type>.Parse(typeof(CreatureTemplate.Type), objectSettings[2], true);
-                            bool dead = Convert.ToBoolean(objectSettings[3]);
-                            string ID = objectSettings[4];
-                            int amount = Convert.ToInt32(objectSettings[5]);
-                            EntityID entityID = self.game.GetNewID();
-
-                            if (ID != "" && ID.Contains(".")) {
-                                string[] IDSplit = ID.Split('.');
-                                try {
-                                    entityID = new EntityID(Convert.ToInt32(IDSplit[0]), Convert.ToInt32(IDSplit[1]));
-                                } catch (FormatException err) {
-                                    Debug.Log("ID was incorrectly formated");
-                                    Debug.Log(err);
-                                } catch (OverflowException err) {
-                                    Debug.Log("ID was too big");
-                                    Debug.Log(err);
-                                } catch (Exception err) {
-                                    Debug.Log(err);
-                                }
-                            }
-
-                            for (int j = 0; j < amount; j++) {
-                                AbstractCreature abstractCreature = new AbstractCreature(self.world, StaticWorld.GetCreatureTemplate(creatureTemplateType), null, self.GetWorldCoordinate(self.roomSettings.placedObjects[i].pos), entityID);
-                                if (dead) {
-                                    abstractCreature.Die();
-                                }
-                                self.abstractRoom.AddEntity(abstractCreature);
-                            }
-                        }
-                    });
-                } catch (Exception err) {
-                    Plugin.logger.LogDebug("Yup the IL here fails");
-                    Plugin.logger.LogError(err);
-                }
-            }
-            else {
+            if (!cursor.TryGotoPrev(MoveType.After, i => i.MatchLdarg(0))) {
                 Plugin.logger.LogDebug("Pitch Black IL Room Placedobjects failed 2");
                 return;
+            }
+            try {
+                cursor.Emit(OpCodes.Ldarg_0);
+                cursor.Emit(OpCodes.Ldloc, 43);
+                cursor.EmitDelegate((Room self, int i) => {
+                    if (self.roomSettings.placedObjects[i].active && self.roomSettings.placedObjects[i].type == ReliableCreatureSpawner) {
+                        
+                        if (self.game.session is SandboxGameSession session && !session.PlayMode) {
+                            return;
+                        }
+
+                        string[] objectSettings = self.roomSettings.placedObjects[i].data.ToString().Split('~');
+
+                        CreatureTemplate.Type creatureTemplateType = (CreatureTemplate.Type)ExtEnum<CreatureTemplate.Type>.Parse(typeof(CreatureTemplate.Type), objectSettings[2], true);
+                        bool dead = Convert.ToBoolean(objectSettings[3]);
+                        string ID = objectSettings[4];
+                        int amount = Convert.ToInt32(objectSettings[5]);
+                        EntityID entityID = self.game.GetNewID();
+
+                        if (ID != "" && ID.Contains(".")) {
+                            string[] IDSplit = ID.Split('.');
+                            try {
+                                entityID = new EntityID(Convert.ToInt32(IDSplit[0]), Convert.ToInt32(IDSplit[1]));
+                            } catch (FormatException err) {
+                                Debug.Log("ID was incorrectly formated");
+                                Debug.Log(err);
+                            } catch (OverflowException err) {
+                                Debug.Log("ID was too big");
+                                Debug.Log(err);
+                            } catch (Exception err) {
+                                Debug.Log(err);
+                            }
+                        }
+
+                        for (int j = 0; j < amount; j++) {
+                            AbstractCreature abstractCreature = new AbstractCreature(self.world, StaticWorld.GetCreatureTemplate(creatureTemplateType), null, self.GetWorldCoordinate(self.roomSettings.placedObjects[i].pos), entityID);
+                            if (dead) {
+                                abstractCreature.Die();
+                            }
+                            self.abstractRoom.AddEntity(abstractCreature);
+                        }
+                    }
+                });
+            } catch (Exception err) {
+                Plugin.logger.LogDebug("Yup the IL here fails");
+                Plugin.logger.LogError(err);
             }
         } catch (Exception err) {
             Plugin.logger.LogError(err);
