97f508d0339fff683669cc2cd4660b07f28f2275
Lurzard <126424596+Lurzard@users.noreply.github.com>
2024-07-25 00:15:49 -0400

    removing dev stuff (scary I know)
    this is just for the release branch, that way we can push playtest builds via github without including all the stuff we mess wiith and just the mod itself.

--- PitchBlack-master/src/AllowNullAttribute.cs
+++
@@ -1,4 +0,0 @@
-﻿namespace System.Diagnostics.CodeAnalysis;
-
-[AttributeUsage(AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Parameter, Inherited = false)]
-public sealed class AllowNullAttribute : Attribute { }
