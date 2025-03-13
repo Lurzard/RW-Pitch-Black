7c0f3d64192aef8a2aef9654ff8949cdb02bbca8
Moon613 <willzendgraft@gmail.com>
2025-03-13 10:34:00 -0400

    !!!!!!Change to relationship adjusting, using unsafe code!!!!!!
    Ignore it it's fine, the exclamation marks are just so that in the event that the unsafe code causes unforeseen errors down the line it's easier to find which commit it was that made them.

--- PitchBlack-master/src/NightTerror/AdjustNightTerrorRelations.cs
+++ PitchBlack-master/src/NightTerror/AdjustNightTerrorRelations.cs
@@ -1,33 +1,25 @@
+using System;
+using MonoMod.Utils;
+#pragma warning disable CS8500
+
 namespace PitchBlack;
 public class ScareEverything {
-    public static CreatureTemplate.Relationship.Type newRelation = CreatureTemplate.Relationship.Type.Afraid;
-    public static bool Condition(Creature crit) {
-        if (crit != null && crit.Template.type == CreatureTemplateType.NightTerror) {
-            return true;
-        }
-        else {
-            return false;
-        }
+    private static readonly CreatureTemplate.Relationship.Type newRelation = CreatureTemplate.Relationship.Type.Afraid;
+    private static bool Condition(Creature crit) {
+        return crit != null && crit.Template.type == CreatureTemplateType.NightTerror;
     }
-
-    /*public static void SetRelationship<T>(T CreatureAI, CreatureTemplate.Relationship.Type Relationship) where T : ArtificialIntelligence
-    {
-        On.CreatureAI.IUseARelationshipTracker_UpdateDynamicRelationship += (orig, self, dRelation) => {
-
-            CreatureTemplate.Relationship relationship = orig(self, dRelation);
-            Creature trackedCreature = dRelation?.trackerRep?.representedCreature?.realizedCreature;
-            if (Condition(trackedCreature))
-            {
-                return new CreatureTemplate.Relationship(Relationship, 10f);
-            }
-            return relationship;
-        };
-    }*/
-
+    private delegate CreatureTemplate.Relationship Orig(ArtificialIntelligence self, RelationshipTracker.DynamicRelationship dRelation);
+    unsafe private static CreatureTemplate.Relationship AdjustRelationship(Orig* orig, ArtificialIntelligence self, RelationshipTracker.DynamicRelationship dRelation) {
+        CreatureTemplate.Relationship relationship = (*orig)(self, dRelation);
+        Creature trackedCreature = dRelation?.trackerRep?.representedCreature?.realizedCreature;
+        if (trackedCreature != null && trackedCreature.Template.type == CreatureTemplateType.NightTerror) {
+            return new CreatureTemplate.Relationship(newRelation, 10);
+        }
+        return relationship;
+    }
     public static void Apply()
     {
         On.ArtificialIntelligence.DynamicRelationship_CreatureRepresentation_AbstractCreature += (orig, self, rep, absCrit) => {
-            //Debug.Log("Yippe updating");
             Creature trackedCreature = null;
             if (rep != null) {
                 trackedCreature = rep.representedCreature?.realizedCreature;
@@ -35,181 +27,47 @@ public class ScareEverything {
             else if (absCrit != null) {
                 trackedCreature = absCrit.realizedCreature;
             }
-            //Debug.Log($"trackedCreature is: {trackedCreature} Player? {trackedCreature is Player}");
             if (Condition(trackedCreature)) {
-                //Debug.Log($"Returned afraid\n");
                 //self.preyTracker.prey.RemoveAll(c => c.critRep.representedCreature.realizedCreature is Player);
                 return new CreatureTemplate.Relationship(newRelation, 10f);
             }
-            //Debug.Log($"Skipped muh code. self.creature: {self.creature}. realized?: {self.creature.realizedCreature} template: {self.creature.realizedCreature.Template.type}\n");
             return orig(self, rep, absCrit);
         };
-
         On.ArtificialIntelligence.StaticRelationship += (orig, self, otherCreature) => {
-            if (Condition(otherCreature.realizedCreature)) {
-                //Debug.Log($"Returning Afraid\n");
-                return new CreatureTemplate.Relationship(newRelation, 10f);
-            }
-            //Debug.Log($"Did not become afraid\n");
-            return orig(self, otherCreature);
+            return Condition(otherCreature.realizedCreature) ? new CreatureTemplate.Relationship(newRelation, 10f) : orig(self, otherCreature);
         };
-
-        On.BigNeedleWormAI.IUseARelationshipTracker_UpdateDynamicRelationship += (orig, self, dRelation) => {
-            CreatureTemplate.Relationship relationship = orig(self, dRelation);
-            Creature trackedCreature = dRelation?.trackerRep?.representedCreature?.realizedCreature;
-            if (Condition(trackedCreature)) {
-                //Debug.Log("Made it to changing relationship");
-                return new CreatureTemplate.Relationship(newRelation, 10f);
-            }
-            return relationship;
-        };
-
-        On.BigSpiderAI.IUseARelationshipTracker_UpdateDynamicRelationship += (orig, self, dRelation) => {
-            CreatureTemplate.Relationship relationship = orig(self, dRelation);
-            Creature trackedCreature = dRelation?.trackerRep?.representedCreature?.realizedCreature;
-            if (Condition(trackedCreature)) {
-                //Debug.Log("Made it to changing relationship");
-                return new CreatureTemplate.Relationship(newRelation, 10f);
-            }
-            return relationship;
-        };
-
-        On.CentipedeAI.IUseARelationshipTracker_UpdateDynamicRelationship += (orig, self, dRelation) => {
-            Creature trackedCreature = dRelation?.trackerRep?.representedCreature?.realizedCreature;
-            if (trackedCreature != null && trackedCreature.Template.type == CreatureTemplateType.NightTerror && self.centipede.Template.type != CreatureTemplateType.NightTerror) {
-                // If the 'target' creature is a NightTerror and self is not, be afraid
-                return new CreatureTemplate.Relationship(newRelation, 10f);
-            }
-            if (trackedCreature != null && trackedCreature.Template.type == CreatureTemplate.Type.Slugcat && self.centipede.Template.type == CreatureTemplateType.NightTerror) {
-                // If the 'target' creature is slugcat and it is a nightterror, eat it 100%
-                return new CreatureTemplate.Relationship(CreatureTemplate.Relationship.Type.Eats, 1f);
-            }
-            else if (trackedCreature != null && trackedCreature.Template.type == CreatureTemplateType.NightTerror && self.centipede.Template.type == CreatureTemplateType.NightTerror) {
-                // If the 'target' is a NightTerror, and self is a NightTerror, Ignore it
-                return new CreatureTemplate.Relationship(CreatureTemplate.Relationship.Type.Ignores, 1f);
-            }
-            // If somehow none of the above, return orig
-            return orig(self, dRelation);
-        };
-
-        On.CicadaAI.IUseARelationshipTracker_UpdateDynamicRelationship += (orig, self, dRelation) => {
-            CreatureTemplate.Relationship relationship = orig(self, dRelation);
-            Creature trackedCreature = dRelation?.trackerRep?.representedCreature?.realizedCreature;
-            if (Condition(trackedCreature)) {
-                //Debug.Log("Made it to changing relationship");
-                return new CreatureTemplate.Relationship(newRelation, 10f);
-            }
-            return relationship;
-        };
-
-        On.DropBugAI.IUseARelationshipTracker_UpdateDynamicRelationship += (orig, self, dRelation) => {
-            CreatureTemplate.Relationship relationship = orig(self, dRelation);
-            Creature trackedCreature = dRelation?.trackerRep?.representedCreature?.realizedCreature;
-            if (Condition(trackedCreature)) {
-                //Debug.Log("Made it to changing relationship");
-                return new CreatureTemplate.Relationship(newRelation, 10f);
-            }
-            return relationship;
-        };
-
-        On.JetFishAI.IUseARelationshipTracker_UpdateDynamicRelationship += (orig, self, dRelation) => {
-            CreatureTemplate.Relationship relationship = orig(self, dRelation);
-            Creature trackedCreature = dRelation?.trackerRep?.representedCreature?.realizedCreature;
-            if (Condition(trackedCreature)) {
-                //Debug.Log("Made it to changing relationship");
-                return new CreatureTemplate.Relationship(newRelation, 10f);
-            }
-            return relationship;
-        };
-
-        On.LizardAI.IUseARelationshipTracker_UpdateDynamicRelationship += (orig, self, dRelation) => {
-            CreatureTemplate.Relationship relationship = orig(self, dRelation);
-            Creature trackedCreature = dRelation?.trackerRep?.representedCreature?.realizedCreature;
-            if (Condition(trackedCreature)) {
-                //Debug.Log("Made it to changing relationship");
-                return new CreatureTemplate.Relationship(newRelation, 10f);
-            }
-            return relationship;
-        };
-
-        On.MirosBirdAI.IUseARelationshipTracker_UpdateDynamicRelationship += (orig, self, dRelation) => {
-            CreatureTemplate.Relationship relationship = orig(self, dRelation);
-            Creature trackedCreature = dRelation?.trackerRep?.representedCreature?.realizedCreature;
-            if (Condition(trackedCreature)) {
-                //Debug.Log("Made it to changing relationship");
-                return new CreatureTemplate.Relationship(newRelation, 10f);
-            }
-            return relationship;
-        };
-
-        On.ScavengerAI.IUseARelationshipTracker_UpdateDynamicRelationship += (orig, self, dRelation) => {
-            CreatureTemplate.Relationship relationship = orig(self, dRelation);
-            Creature trackedCreature = dRelation?.trackerRep?.representedCreature?.realizedCreature;
-            if (Condition(trackedCreature)) {
-                //Debug.Log("Made it to changing relationship");
-                return new CreatureTemplate.Relationship(newRelation, 10f);
-            }
-            return relationship;
-        };
-
-        On.TempleGuardAI.IUseARelationshipTracker_UpdateDynamicRelationship += (orig, self, dRelation) => {
-            CreatureTemplate.Relationship relationship = orig(self, dRelation);
-            Creature trackedCreature = dRelation?.trackerRep?.representedCreature?.realizedCreature;
-            if (Condition(trackedCreature)) {
-                //Debug.Log("Made it to changing relationship");
-                return new CreatureTemplate.Relationship(newRelation, 10f);
-            }
-            return relationship;
-        };
-
-        On.VultureAI.IUseARelationshipTracker_UpdateDynamicRelationship += (orig, self, dRelation) => {
-            CreatureTemplate.Relationship relationship = orig(self, dRelation);
-            Creature trackedCreature = dRelation?.trackerRep?.representedCreature?.realizedCreature;
-            if (Condition(trackedCreature)) {
-                //Debug.Log("Made it to changing relationship");
-                return new CreatureTemplate.Relationship(newRelation, 10f);
-            }
-            return relationship;
-        };
-
-        On.MoreSlugcats.InspectorAI.IUseARelationshipTracker_UpdateDynamicRelationship += (orig, self, dRelation) => {
-            CreatureTemplate.Relationship relationship = orig(self, dRelation);
-            Creature trackedCreature = dRelation?.trackerRep?.representedCreature?.realizedCreature;
-            if (Condition(trackedCreature)) {
-                //Debug.Log("Made it to changing relationship");
-                return new CreatureTemplate.Relationship(newRelation, 10f);
-            }
-            return relationship;
-        };
-
-        On.MoreSlugcats.SlugNPCAI.IUseARelationshipTracker_UpdateDynamicRelationship += (orig, self, dRelation) => {
-            CreatureTemplate.Relationship relationship = orig(self, dRelation);
-            Creature trackedCreature = dRelation?.trackerRep?.representedCreature?.realizedCreature;
-            if (Condition(trackedCreature)) {
-                //Debug.Log("Made it to changing relationship");
-                return new CreatureTemplate.Relationship(newRelation, 10f);
-            }
-            return relationship;
-        };
-
-        On.MoreSlugcats.StowawayBugAI.IUseARelationshipTracker_UpdateDynamicRelationship += (orig, self, dRelation) => {
-            CreatureTemplate.Relationship relationship = orig(self, dRelation);
-            Creature trackedCreature = dRelation?.trackerRep?.representedCreature?.realizedCreature;
-            if (Condition(trackedCreature)) {
-                //Debug.Log("Made it to changing relationship");
-                return new CreatureTemplate.Relationship(newRelation, 10f);
-            }
-            return relationship;
-        };
-
-        On.MoreSlugcats.YeekAI.IUseARelationshipTracker_UpdateDynamicRelationship += (orig, self, dRelation) => {
-            CreatureTemplate.Relationship relationship = orig(self, dRelation);
-            Creature trackedCreature = dRelation?.trackerRep?.representedCreature?.realizedCreature;
-            if (Condition(trackedCreature)) {
-                //Debug.Log("Made it to changing relationship");
-                return new CreatureTemplate.Relationship(newRelation, 10f);
-            }
-            return relationship;
-        };
+        unsafe {
+            On.BigNeedleWormAI.IUseARelationshipTracker_UpdateDynamicRelationship += (orig, self, dRelation) => AdjustRelationship((Orig*)&orig, self, dRelation);
+            On.BigSpiderAI.IUseARelationshipTracker_UpdateDynamicRelationship += (orig, self, dRelation) => AdjustRelationship((Orig*)&orig, self, dRelation);
+            On.CentipedeAI.IUseARelationshipTracker_UpdateDynamicRelationship += (orig, self, dRelation) => {
+                Creature trackedCreature = dRelation?.trackerRep?.representedCreature?.realizedCreature;
+                if (trackedCreature != null && trackedCreature.Template.type == CreatureTemplateType.NightTerror && self.centipede.Template.type != CreatureTemplateType.NightTerror) {
+                    // If the 'target' creature is a NightTerror and self is not, be afraid
+                    return new CreatureTemplate.Relationship(newRelation, 10f);
+                }
+                if (trackedCreature != null && trackedCreature.Template.type == CreatureTemplate.Type.Slugcat && self.centipede.Template.type == CreatureTemplateType.NightTerror) {
+                    // If the 'target' creature is slugcat and it is a nightterror, eat it 100%
+                    return new CreatureTemplate.Relationship(CreatureTemplate.Relationship.Type.Eats, 1f);
+                }
+                else if (trackedCreature != null && trackedCreature.Template.type == CreatureTemplateType.NightTerror && self.centipede.Template.type == CreatureTemplateType.NightTerror) {
+                    // If the 'target' is a NightTerror, and self is a NightTerror, Ignore it
+                    return new CreatureTemplate.Relationship(CreatureTemplate.Relationship.Type.Ignores, 1f);
+                }
+                // If somehow none of the above, return orig
+                return orig(self, dRelation);
+            };
+            On.CicadaAI.IUseARelationshipTracker_UpdateDynamicRelationship += (orig, self, dRelation) => AdjustRelationship((Orig*)&orig, self, dRelation);
+            On.DropBugAI.IUseARelationshipTracker_UpdateDynamicRelationship += (orig, self, dRelation) => AdjustRelationship((Orig*)&orig, self, dRelation);
+            On.JetFishAI.IUseARelationshipTracker_UpdateDynamicRelationship += (orig, self, dRelation) => AdjustRelationship((Orig*)&orig, self, dRelation);
+            On.LizardAI.IUseARelationshipTracker_UpdateDynamicRelationship += (orig, self, dRelation) => AdjustRelationship((Orig*)&orig, self, dRelation);
+            On.MirosBirdAI.IUseARelationshipTracker_UpdateDynamicRelationship += (orig, self, dRelation) => AdjustRelationship((Orig*)&orig, self, dRelation);
+            On.ScavengerAI.IUseARelationshipTracker_UpdateDynamicRelationship += (orig, self, dRelation) => AdjustRelationship((Orig*)&orig, self, dRelation);
+            On.TempleGuardAI.IUseARelationshipTracker_UpdateDynamicRelationship += (orig, self, dRelation) => AdjustRelationship((Orig*)&orig, self, dRelation);
+            On.VultureAI.IUseARelationshipTracker_UpdateDynamicRelationship += (orig, self, dRelation) => AdjustRelationship((Orig*)&orig, self, dRelation);
+            On.MoreSlugcats.InspectorAI.IUseARelationshipTracker_UpdateDynamicRelationship += (orig, self, dRelation) => AdjustRelationship((Orig*)&orig, self, dRelation);
+            On.MoreSlugcats.SlugNPCAI.IUseARelationshipTracker_UpdateDynamicRelationship += (orig, self, dRelation) => AdjustRelationship((Orig*)&orig, self, dRelation);
+            On.MoreSlugcats.StowawayBugAI.IUseARelationshipTracker_UpdateDynamicRelationship += (orig, self, dRelation) => AdjustRelationship((Orig*)&orig, self, dRelation);
+            On.MoreSlugcats.YeekAI.IUseARelationshipTracker_UpdateDynamicRelationship += (orig, self, dRelation) => AdjustRelationship((Orig*)&orig, self, dRelation);
+        }
     }
 }
