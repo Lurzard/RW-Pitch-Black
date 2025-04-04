193df3dc2502190e8c01e6ecc26f1efcec6c9634
Lurzard <126424596+Lurzard@users.noreply.github.com>
2025-04-04 12:12:08 -0400

    Update for thanatosis (untested)

--- PitchBlack-master/src/Scug Code/Beacon/Thanatosis.cs
+++ PitchBlack-master/src/Scug Code/Beacon/Thanatosis.cs
@@ -11,29 +11,14 @@ namespace PitchBlack;
 
 public class Thanatosis
 {
-
     public static void Apply() {
         On.Player.Update += Player_Update;
-        //On.Player.Die += Player_Die;
-        //On.Creature.Die += Creature_Die;
     }
-
-    //private static void Creature_Die(On.Creature.orig_Die orig, Creature self)
-    //{
-    //    orig(self);
-    //    if (player.SlugCatClass != Plugin.Beacon) return;
-    //    CreatureDieAndThanatosis();
-    //}
-
-    //private static void Player_Die(On.Player.orig_Die orig, Player self) {
-    //    orig(self);
-    //    if (self.SlugCatClass != Plugin.Beacon) return;
-    //    DieAndThanatosis();
-    //}
 
     private static void Player_Update(On.Player.orig_Update orig, Player self, bool eu) {
         orig(self, eu);
         BeaconUpdate();
+        //this will error if everything inside is not also static
     }
 
     //the hooks need static to access our methods
@@ -43,19 +28,14 @@ public class Thanatosis
 
     public static Player player; //self.
     public static Creature creature; //base.
-    public static RainWorldGame rwg; //used to call GameOver
 
     //*******************
     //THANATOSIS TRACKING
     //*******************
 
-    public static int usedThanatosis;
-
     //these should probably be moved to BeaconCWT, but they need to be static rn
     public static bool isDead; //state tracking
     public static int inThanatosisTime; //tracking current time spent in Thanatosis
-    public static int startingThanatosisOnActivate = -1; //actually determine which state to enter
-    public static bool inputNeedsReset;
 
     public static float ThanatosisLimit {
         get {
@@ -65,18 +45,9 @@ public class Thanatosis
 
     public static void BeaconUpdate() {
         if (Plugin.scugCWT.TryGetValue(player, out ScugCWT scugCWT) && scugCWT is BeaconCWT beaconCWT) {
-            if (!player.input[0].jmp) {
-                inputNeedsReset = false;
-            }
             //both inputs will be changed to spec eventually, I'll say that when I try, the code doesn't know what it is!
             if  (player.input[0].jmp) {
-                if (startingThanatosisOnActivate == -1) {
-                    startingThanatosisOnActivate = isDead ? 1 : 0;
-                }
-
-                if (startingThanatosisOnActivate == 0) {
-                    ToggleThanatosis(); //butterfly effects into DieAndThanatosis
-                }
+                ToggleThanatosis(); //butterfly effects into DieAndThanatosis
             }
             ThanatosisUpdate();
         }
@@ -86,8 +57,10 @@ public class Thanatosis
         if (Plugin.scugCWT.TryGetValue(player, out ScugCWT scugCWT) && scugCWT is BeaconCWT beaconCWT) {
             if (isDead) inThanatosisTime++;
             if (inThanatosisTime >= ThanatosisLimit) {
-                    Debug.Log("WAAAIIIIT DONT DO THAT!!!");
-                    rwg.GameOver(null);
+                Debug.Log("WAAAIIIIT DONT DO THAT!!!");
+                creature.dead = true;
+                isDead = false;
+                //rwg.GameOver(null);
                 }
             else inThanatosisTime = 0;
         }
@@ -103,8 +76,6 @@ public class Thanatosis
             //sound design for isDead true or false :cooking:
             player.room.PlaySound(MoreSlugcatsEnums.MSCSoundID.Ghost_Ping_Base, player.mainBodyChunk);
             player.room.PlaySound( isDead ? SoundID.In_Room_Deer_Summoned : SoundID.Distant_Deer_Summoned, player.mainBodyChunk, false, 0.35f, UnityEngine.Random.value * 0.5f + 0.8f);
-            player.Blink(isDead ? 40 : 20);
-            player.Stun(isDead ? 20 : 10);
             //creature.dead = isDead ? true : false; //theoretically echo into all the death code? but unfortunately that calls GameOver
             DieAndThanatosis(); //completely seperate implementation of player.Die();
         }
@@ -120,12 +91,14 @@ public class Thanatosis
                     realizedRoom.game.GameOver(null);
                     realizedRoom.PlaySound(SoundID.UI_Slugcat_Die, creature.mainBodyChunk);
                 }
-                if (isDead) { //Creature.Die code
+                if (isDead) { //Creature.Die
+                    player.Blink(20);
                     creature.dead = true;
                     creature.LoseAllGrasps();
                     creature.abstractCreature.Die();
                 }
                 else { //BigSpider revive
+                    player.Stun(20);
                     creature.dead = false;
                     creature.abstractCreature.abstractAI.SetDestination(creature.abstractCreature.pos);
                 }
